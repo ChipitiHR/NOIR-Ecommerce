c64bf01e18889f25990965798b04e1c01dc3e3c6
ChipitiHR <s.hurtado003@ebc.edu.mx>
2026-09-21 23:09:03 -0600

    Agregar JavaScript de producto en carpeta correcta

---
+++ js/producto.js
@@ -0,0 +1,4 @@
+// Archivo puente para conservar la estructura del proyecto
+const noirProductScript = document.createElement("script");
+noirProductScript.src = "../producto.js";
+document.head.appendChild(noirProductScript);
