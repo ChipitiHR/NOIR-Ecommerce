9108643a9ad73629f70d6d1beeb1e22dcbc8a91e
ChipitiHR <s.hurtado003@ebc.edu.mx>
2026-09-21 23:08:59 -0600

    Agregar JavaScript principal en carpeta correcta

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Archivo puente para conservar la estructura del proyecto
+const noirMainScript = document.createElement("script");
+noirMainScript.src = "../script.js";
+document.head.appendChild(noirMainScript);
